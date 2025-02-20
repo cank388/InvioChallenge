e222f2855dbfdc803667bb73cf5d0d23bd2f7730
Can Kalender <cankalender.tr@gmail.com>
2025-02-20 13:54:55 +0300

    initial commit.

---
+++ universitylisting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  universitylisting
+//
+//  Created by Can Kalender on 20.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view. init
+    }
+
+
+}
+
